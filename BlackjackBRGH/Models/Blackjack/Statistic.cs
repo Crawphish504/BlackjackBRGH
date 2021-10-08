cd911dd9526db1eff6343b010192a59d1da287f7
Crawphish504 <nola_branch@yahoo.com>
2021-10-08 13:56:00 -0500

    Add project files.

---
+++ BlackjackBRGH/Models/Blackjack/Statistic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackjackBRGH.Models.Blackjack
+{
+    public class Statistic
+    {
+
+        //Statistics
+        public static int DealerWins { get; private set; } = 0;
+        public static int PlayerWins { get; private set; } = 0;
+        public static int Draws { get; private set; } = 0;
+
+
+                
+        // Reset Statistics    
+        public static void ResetStat()
+        {
+            DealerWins = 0;
+            PlayerWins = 0;
+            Draws = 0;
+        }
+
+        public static void AddPlayerWin()
+        {
+            PlayerWins++;
+        }
+        public static void AddDealerWin()
+        {
+            DealerWins++;
+        }
+        public static void AddDraw()
+        {
+            Draws++;
+        }
+
+    }
+}
